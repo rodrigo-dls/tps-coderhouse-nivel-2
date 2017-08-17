73ebd197429c83f6578a49a535dd5098a14843ad
Rodrigo de los Santos <rodri.m.dls@gmail.com>
2017-08-17 11:00:58 -0300

    Calculadora basica.

---
+++ tp-1/js/tp-1.js
@@ -0,0 +1,21 @@
+// Calculadora basica
+
+var primerNumero = Number(prompt("Ingrese el primer numero", "0"))
+
+var segundoNumero = Number(prompt("Ingrese el segundo numero", "1"))
+
+var sumar = primerNumero + segundoNumero
+
+var restar = primerNumero - segundoNumero
+
+var multiplicar = primerNumero * segundoNumero
+
+var dividir = primerNumero / segundoNumero
+
+alert(sumar)
+
+alert(restar)
+
+alert(multiplicar)
+
+alert(dividir)
